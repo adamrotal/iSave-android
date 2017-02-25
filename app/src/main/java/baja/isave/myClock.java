df85f77043c75949905e6db075a99398e67a6c84
Ahmad Fajar Prasetiyo <ahmad.fajar260695@gmail.com>
2017-02-25 16:02:51 +0700

    location

--- app/src/main/java/baja/isave/myClock.java
+++ app/src/main/java/baja/isave/myClock.java
@@ -1,22 +1,136 @@
 package baja.isave;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class myClock extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class myClock extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
         setContentView(R.layout.activity_my_clock);
     }
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
 
-    public void alarmPressed (View view){
-        Intent i = new Intent(myClock.this,myAlarm.class);
+    protected void onResume() {
+        super.onResume();
+        this.onConnected(null);
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    public void alarmPressed(View view) {
+        Intent i = new Intent(myClock.this, myAlarm.class);
         startActivity(i);
-//        finish();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        final int REQUEST_LOCATION = 99;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+//            Context context = getApplicationContext();
+//            CharSequence text = "permission denied";
+//            int duration = Toast.LENGTH_LONG;
+//            Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+
+            return;
+        }
+
+        System.out.println("aaa");
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        Context context = getApplicationContext();
+        System.out.println("bbb");
+        Geocoder gcd = new Geocoder(context, Locale.getDefault());
+        System.out.println("ccc");
+
+        LocationManager locationManager = (LocationManager)  this.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
+
+        //You can still do this if you like, you might get lucky:
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        if (location != null) {
+
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+//            Toast.makeText(context, "latitude:" + latitude + " longitude:" + longitude, Toast.LENGTH_SHORT).show();
+            List<Address> addresses = null;
+
+            try {
+                addresses = gcd.getFromLocation(latitude, longitude, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (addresses.size() > 0)
+            {
+                TextView mTextViewLocation = (TextView)findViewById(R.id.location);
+                mTextViewLocation.setText(addresses.get(0).getLocality());
+//                System.out.println(addresses.get(0).getLocality());
+            }
+        }
+
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
