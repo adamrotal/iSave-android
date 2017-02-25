4ebf62c78eeaa4137abc175d69cf6456bfebb18c
adamrotal <adamrotal@gmail.com>
2017-02-25 19:53:23 +0700

    add shake fitur

--- app/src/main/java/baja/isave/dismissAlarm.java
+++ app/src/main/java/baja/isave/dismissAlarm.java
@@ -7,6 +7,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -15,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Rotal on 2/25/2017.
@@ -24,6 +26,8 @@ public class dismissAlarm extends AppCompatActivity implements SensorEventListen
     TextView secInfo;
 
     ImageButton btnStop;
+    MediaPlayer player;
+
 
     Ringtone ringTone;
     public static final String PREFS_NAME = "settings";
@@ -34,51 +38,57 @@ public class dismissAlarm extends AppCompatActivity implements SensorEventListen
     public static final String TYPE_MATH = "4";
 
     private SensorManager mSensorManager;
-    private Sensor mProximity;
+    private Sensor mProximity, mAccelerometer;
+
+    private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+    private static final int SHAKE_SLOP_TIME_MS = 500;
+    private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
+
+    private OnShakeListener mListener;
+    private long mShakeTimestamp;
+    private int mShakeCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         String type = settings.getString(KEY_ID_TYPE_ALARM,TYPE_DEFAULT);
 
         if(type.equals(TYPE_DEFAULT)) {
             setContentView(R.layout.activity_default);
         } else if (type.equals(TYPE_FLIP)) {
+            mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
             setContentView(R.layout.activity_flip);
         } else if (type.equals(TYPE_MATH)) {
             setContentView(R.layout.activity_math);
-        } else {
+        } else { // Shake
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
             setContentView(R.layout.activity_shake);
         }
 
-
-//        btnStop = (ImageButton)findViewById(R.id.imageButton);
 
         String stringUri = getIntent().getStringExtra("SEC_RINGTONE_URI");
         Uri uri = Uri.parse(stringUri);
 
         ringTone = RingtoneManager.getRingtone(getApplicationContext(), uri);
-//        secInfo.append(ringTone.getTitle(dismissAlarm.this));
 
-        ringTone.play();
+        player = MediaPlayer.create(this, uri);
+        player.setLooping(true);
+        player.start();
 
-//        btnStop.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                if(ringTone != null){
-//                    ringTone.stop();
-//                    ringTone = null;
-//                }
-//
-//                Intent i = new Intent(dismissAlarm.this,myAlarm.class);
-//                startActivity(i);
-//                finish();
-//            }
-//        });
+    }
 
+    public void setOnShakeListener(OnShakeListener listener) {
+        this.mListener = listener;
+    }
+
+    public interface OnShakeListener {
+        void onShake(int count);
     }
 
     @Override
@@ -93,11 +103,16 @@ public class dismissAlarm extends AppCompatActivity implements SensorEventListen
         mSensorManager.unregisterListener(this);
     }
     
-    public void listenerDefault(View view){
-        if(ringTone != null){
-            ringTone.stop();
-            ringTone = null;
+    public void listenerDefault(){
+        if (player != null) {
+            player.stop();
+            player = null;
         }
+
+//        if(ringTone != null){
+//            ringTone.stop();
+//            ringTone = null;
+//        }
 
         Intent i = new Intent(dismissAlarm.this,myAlarm.class);
         startActivity(i);
@@ -105,7 +120,14 @@ public class dismissAlarm extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
     public void onSensorChanged(SensorEvent event) {
+        System.out.println("chnged");
+
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         String type = settings.getString(KEY_ID_TYPE_ALARM,TYPE_DEFAULT);
 
@@ -113,24 +135,49 @@ public class dismissAlarm extends AppCompatActivity implements SensorEventListen
             System.out.println("ini masuk sensor proximity");
             if (event.values[0] >= -0.01 && event.values[0] <= 0.01) {
                 if(type.equals(TYPE_FLIP)) {
-                    if (ringTone != null) {
-                        ringTone.stop();
-                        ringTone = null;
-                    }
-
-                    Intent i = new Intent(dismissAlarm.this, myAlarm.class);
-                    startActivity(i);
-                    finish();
+                    listenerDefault();
                 }
             }
+        } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.out.println("Count : " + mShakeCount);
+            if (mListener != null) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                float gX = x / SensorManager.GRAVITY_EARTH;
+                float gY = y / SensorManager.GRAVITY_EARTH;
+                float gZ = z / SensorManager.GRAVITY_EARTH;
+
+                // gForce will be close to 1 when there is no movement.
+                float gForce = (float) Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+//            System.out.println("Shake me : " + gForce);
+
+                if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                    final long now = System.currentTimeMillis();
+                    // ignore shake events too close to each other (500ms)
+                    if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                        return;
+                    }
+
+                    // reset the shake count after 3 seconds of no shakes
+//                if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+//                    mShakeCount = 0;
+//                }
+
+                    mShakeTimestamp = now;
+                    mShakeCount++;
+                    System.out.println(mShakeCount);
+                    if (mShakeCount >= 12) {
+//                        mListener.onShake(mShakeCount);
+                        Toast.makeText(dismissAlarm.this,"Your Message", Toast.LENGTH_LONG).show();
+                        listenerDefault();
+                    }
+                }
+            }
+
         }
     }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-
 
 }
